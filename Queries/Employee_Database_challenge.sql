12e65c55ef5e1770a74e29aaf0498c04bf614b1d
Rus Kram <ruskram87@gmail.com>
2022-01-23 20:18:48 -0500

    Typo update

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -4,13 +4,13 @@ SELECT e.emp_no,
     ti.title,
 	ti.from_date,
     ti.to_date
-INTO retierment_titles
+INTO retirement_titles
 FROM employees as e
 INNER JOIN titles as ti
 ON (e.emp_no = ti.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31');
 
-SELECT * FROM retierment_titles
+SELECT * FROM retirement_titles
 ORDER BY emp_no ASC;
 
 SELECT DISTINCT ON (emp_no) rt.emp_no,
